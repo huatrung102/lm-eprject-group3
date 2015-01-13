4b08a779f1f43587215983b087f28a2fd148c47c
huatrung102 <huatrung102@c9e91d32-ae40-dc30-4ecf-2c78fb3afdd0>
2015-01-13 10:54:43 +0000

    update book copy irbook model

--- lm-eproject-sem2/src/Model/Books.java
+++ lm-eproject-sem2/src/Model/Books.java
@@ -22,23 +22,10 @@ public class Books {
     public String Book_CreateDate;
     public boolean Book_isDeleted;
     public String Cat_Name;
+    public int Book_Count;
     public Books(){
     
-    }
-    public static Books getTestBook(){
-        Books book = new Books();
-        book.Book_Author = "Cornelia Funke";
-        book.Book_Content = "One cruel night, Meggie's father reads aloud from a book called \"Inkheart,\" and an evil ruler escapes the boundaries of fiction and lands in their living room. Suddenly, Meggie is smack in the middle of the kind of adventure she has only read about in books";
-        book.Book_ISBN = "9780439709101";
-        book.Book_ImageFile = "/imgBook/book (6).JPG";
-        book.Book_Language = "English";
-        book.Book_Price = 80;
-        book.Book_Publisher = "Scholastic Paperbacks";
-        book.Book_Title = "Inkheart";
-        book.Cat_Name = "Fiction";
-        return book;
-    }
-    
+    }    
     
     
 }
--- lm-eproject-sem2/src/Model/Members.java
+++ lm-eproject-sem2/src/Model/Members.java
@@ -5,17 +5,20 @@
  */
 package Model;
 
+import Helpers.SqlHelper;
 import SysController.MessageHandle;
+import java.sql.ResultSet;
 import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author Administrator PC
  */
-public class Member {
+public class Members {
     public String Mem_Id;
     public String Mem_FirstName;
-    public String Mem_LastName;    
+    public String Mem_LastName;   
+    public String Mem_No;
     public String Mem_Phone;
     public String Mem_Address;
     public String Mem_Email;
@@ -23,22 +26,23 @@ public class Member {
     public String Mem_CreateDate;
     public String Mem_ImageFile;
     public boolean Mem_isDelete;
+    
     private static String test_col[] = {"Member No","Fullname","Email","Phone"};
     
-    public Member(){
+    public Members(){
     }
      public static DefaultTableModel getTestMemberList(){
         DefaultTableModel tblM = new DefaultTableModel(test_col, 0);         
         
-         MessageHandle.getMessage(MessageHandle.Obj_Member, MessageHandle.Action_insert, 1);
+        
         tblM.addRow(new Object[]{"M0000001","Hua Tran Huu Trung","trunghth@gmail.com","0934399664"});
         tblM.addRow(new Object[]{"M0000002","Ngo Thanh Dat","trunghth1@gmail.com",""});
         tblM.addRow(new Object[]{"M0000003","Tra Phuc Vinh Uy","trunghth2@gmail.com","0123456789"});
         return tblM;
         
     }
-    public static Member getTestMember(){
-        Member mem = new Member();
+    public static Members getTestMember(){
+        Members mem = new Members();
         mem.Mem_Address = "Address ABC";
         mem.Mem_CreateDate = "30/12/2014";
         mem.Mem_Email = "huatrung102@gmail.com";
@@ -49,5 +53,30 @@ public class Member {
         mem.Mem_Status = true;
         mem.Mem_isDelete = false;
         return mem;
+    }
+    //trunghth
+    public static Members getIRCountInformation(String mem_No){
+        Members mem = null;
+        ResultSet rs = null;
+        try {
+            rs = SqlHelper.getResultSet("Members_GetIRCountInformation", mem_No);
+            if(rs.next()){
+                mem = new Members();
+                mem.Mem_Address = rs.getString("Mem_Address");
+                mem.Mem_CreateDate = rs.getString("Mem_CreateDate");
+                mem.Mem_Email = rs.getString("Mem_Email");
+                mem.Mem_FirstName = rs.getString("Mem_FirstName");
+                mem.Mem_Id = rs.getString("Mem_Id");
+                mem.Mem_ImageFile = rs.getString("Mem_ImageFile");
+                mem.Mem_LastName = rs.getString("Mem_LastName");
+                mem.Mem_No = rs.getString("Mem_No");
+                mem.Mem_Phone = rs.getString("Mem_Phone");
+                mem.Mem_Status = rs.getBoolean("Mem_Status");                
+            }
+        } catch (Exception e) {
+            SqlHelper.closeConnection(rs);
+            mem = null;
+        }
+        return mem;        
     }
 }
