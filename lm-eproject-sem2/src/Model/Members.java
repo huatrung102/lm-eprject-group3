287f6962ab854a08d6e1be33e31d7e24ffd41757
huatrung102 <huatrung102@c9e91d32-ae40-dc30-4ecf-2c78fb3afdd0>
2015-01-13 11:04:42 +0000

    update Members.java

--- lm-eproject-sem2/src/Model/Members.java
+++ lm-eproject-sem2/src/Model/Members.java
@@ -27,33 +27,11 @@ public class Members {
     public String Mem_ImageFile;
     public boolean Mem_isDelete;
     
-    private static String test_col[] = {"Member No","Fullname","Email","Phone"};
+    
     
     public Members(){
     }
-     public static DefaultTableModel getTestMemberList(){
-        DefaultTableModel tblM = new DefaultTableModel(test_col, 0);         
-        
-        
-        tblM.addRow(new Object[]{"M0000001","Hua Tran Huu Trung","trunghth@gmail.com","0934399664"});
-        tblM.addRow(new Object[]{"M0000002","Ngo Thanh Dat","trunghth1@gmail.com",""});
-        tblM.addRow(new Object[]{"M0000003","Tra Phuc Vinh Uy","trunghth2@gmail.com","0123456789"});
-        return tblM;
-        
-    }
-    public static Members getTestMember(){
-        Members mem = new Members();
-        mem.Mem_Address = "Address ABC";
-        mem.Mem_CreateDate = "30/12/2014";
-        mem.Mem_Email = "huatrung102@gmail.com";
-        mem.Mem_FirstName = "Hua Tran";
-        mem.Mem_LastName = "Huu Trung";
-        mem.Mem_ImageFile = "/imgMem/anh (11).jpg";
-        mem.Mem_Phone = "0934399664";
-        mem.Mem_Status = true;
-        mem.Mem_isDelete = false;
-        return mem;
-    }
+     
     //trunghth
     public static Members getIRCountInformation(String mem_No){
         Members mem = null;
